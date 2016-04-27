aaa866995c4fe22c5dd6fbad023a196667c0632d
Charles Severance <csev@umich.edu>
2016-04-27 11:44:22 -0400

    Change config.php

--- config.php
+++ config.php
@@ -1,3 +1,6 @@
 <?php
 
-require_once __DIR__."/../tsugi/config.php";
+// Make this require relative to the parent of the current folder
+// http://stackoverflow.com/questions/24753758
+
+require_once dirname(__DIR__)."/tsugi/config.php";
