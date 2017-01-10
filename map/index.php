c4ca04028adb7ca4c1bbafc7733bd28a5388b2f1
Charles Severance <csev@umich.edu>
2017-01-10 12:21:46 -0500

    Fix the map.

--- map/index.php
+++ map/index.php
@@ -4,7 +4,7 @@ require_once "../config.php";
 use \Tsugi\Core\LTIX;
 
 // Sanity checks
-$LAUNCH = LTIX::requireData();
+$LTI = LTIX::requireData(array('user_id', 'link_id', 'role','context_id'));
 
 $p = $CFG->dbprefix;
 if ( isset($_POST['lat']) && isset($_POST['lng']) ) {
@@ -13,51 +13,69 @@ if ( isset($_POST['lat']) && isset($_POST['lng']) ) {
         header( 'Location: '.addSession('index.php') ) ;
         return;
     }
-    $sql = "INSERT INTO {$p}sample_map
-        (context_id, user_id, lat, lng, updated_at)
-        VALUES ( :CID, :UID, :LAT, :LNG, NOW() )
-        ON DUPLICATE KEY
-        UPDATE lat = :LAT, lng = :LNG, updated_at = NOW()";
-    $stmt = $PDOX->prepare($sql);
-    $stmt->execute(array(
-        ':CID' => $CONTEXT->id,
-        ':UID' => $USER->id,
-        ':LAT' => $_POST['lat'],
-        ':LNG' => $_POST['lng']));
+    $stmt = $PDOX->queryDie(
+        "INSERT INTO {$p}sample_map 
+            (context_id, user_id, lat, lng, updated_at) 
+            VALUES ( :CID, :UID, :LAT, :LNG, NOW() ) 
+            ON DUPLICATE KEY 
+            UPDATE lat = :LAT, lng = :LNG, updated_at = NOW()",
+        array(
+            ':CID' => $CONTEXT->id, ':UID' => $USER->id,
+            ':LAT' => $_POST['lat'], ':LNG' => $_POST['lng']));
     $_SESSION['success'] = 'Location updated...';
     header( 'Location: '.addSession('index.php') ) ;
     return;
 }
 
 // Retrieve our row
-$stmt = $PDOX->prepare("SELECT lat,lng FROM {$p}sample_map
-        WHERE context_id = :CID AND user_id = :UID");
-$stmt->execute(array(":CID" => $CONTEXT->id, ":UID" => $USER->id));
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$row = $PDOX->rowDie("SELECT lat,lng FROM {$p}sample_map 
+        WHERE context_id = :CID AND user_id = :UID",
+    array(":CID" => $CONTEXT->id, ":UID" => $USER->id)
+);
+
 // The default for latitude and longitude
 $lat = 42.279070216140425;
 $lng = -83.73981015789798;
 if ( $row !== false ) {
-    if ( abs($row['lat']) <= 90 ) $lat = $row['lat'];
-    if ( abs($row['lng']) <= 180 ) $lng = $row['lng'];
+    $lat = $row['lat'];
+    $lng = $row['lng'];
 }
 
 //Retrieve the other rows
-$stmt = $PDOX->prepare("SELECT lat,lng,displayname FROM {$p}sample_map
+$stmt = $PDOX->prepare("SELECT lat,lng,displayname FROM {$p}sample_map 
         JOIN {$p}lti_user
         ON {$p}sample_map.user_id = {$p}lti_user.user_id
         WHERE context_id = :CID AND {$p}sample_map.user_id <> :UID");
 $stmt->execute(array(":CID" => $CONTEXT->id, ":UID" => $USER->id));
 $points = array();
 while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
-    if ( abs($row['lat']) > 90 ) $row['lat'] = 89;
-    if ( abs($row['lng']) > 180 ) $row['lng'] = 179;
-    $points[] = array($row['lat']+0.0,$row['lng']+0.0);
+    $points[] = array($row['lat']+0.0,$row['lng']+0.0,$row['displayname']);
 }
 
 $OUTPUT->header();
+$OUTPUT->bodyStart();
+$OUTPUT->topNav();
+$OUTPUT->flashMessages();
+
+if ( !isset($CFG->google_map_api_key) ) {
+    echo('<p>There is no MAP api key ($CFG->google_map_api_key)</p>'."\n");
+    $OUTPUT->footer();
+    return;
+}
+
 ?>
-<script src="//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+<div id="map_canvas" style="margin: 10px; width:500px; max-width: 100%; height:500px"></div>
+<form method="post">
+ Latitude: <input size="30" type="text" id="latbox" name="lat" 
+  <?php echo(' value="'.htmlent_utf8($lat).'" '); ?> >
+ Longitude: <input size="30" type="text" id="lngbox" name="lng"
+  <?php echo(' value="'.htmlent_utf8($lng).'" '); ?> >
+ <button type="submit">Save Location</button>
+</form>
+<?php
+$OUTPUT->footerStart();
+?>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=<?= $CFG->google_map_api_key ?>"></script>
 <script type="text/javascript">
 var map;
 
@@ -72,11 +90,11 @@ function initialize_map() {
      mapTypeId: google.maps.MapTypeId.ROADMAP
   }
 
-  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
 
   var marker = new google.maps.Marker({
     draggable: true,
-    position: myLatlng,
+    position: myLatlng, 
     map: map,
     title: "Your location"
   });
@@ -84,6 +102,7 @@ function initialize_map() {
   google.maps.event.addListener(marker, 'dragend', function (event) {
     // getPosition returns a google.maps.LatLng class for
     // for the dropped marker
+    // Make sure to watch the console to see the structure of the event
     window.console && console.log(this.getPosition());
     // TODO: Fix these next two lines - search the web for a solution
     document.getElementById("latbox").value = 71.0;
@@ -108,28 +127,15 @@ function initialize_map() {
   }
 }
 
-// Load the other points
-other_points =
-<?php echo( json_encode($points));?>
+// Load the other points 
+other_points = 
+<?= json_encode($points) ?> 
 ;
-</script>
-<?php
-$OUTPUT->bodyStart();
-$OUTPUT->flashMessages();
-?>
-<div id="map_canvas" style="margin: 10px; width:500px; max-width: 100%; height:500px"></div>
-<form method="post">
- Latitude: <input size="30" type="text" id="latbox" name="lat"
-  <?php echo(' value="'.htmlent_utf8($lat).'" '); ?> >
- Longitude: <input size="30" type="text" id="lngbox" name="lng"
-  <?php echo(' value="'.htmlent_utf8($lng).'" '); ?> >
- <button type="submit">Save Location</button>
-</form>
-<?php
-$OUTPUT->footerStart();
-?>
-<script type="text/javascript">
+
+// Ask jQuery to run our function once the document has loaded
+$(document).ready(function() {
     initialize_map();
+});
 </script>
 <?php
 $OUTPUT->footerEnd();
