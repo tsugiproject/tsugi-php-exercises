26dfb0ebcb9bd4a3b6df54696b8ef959de4a8201
Charles Severance <csev@umich.edu>
2017-01-15 14:01:50 -0500

    Update the documentation.

--- index.php
+++ index.php
@@ -5,36 +5,40 @@
 <body style="font-family: sans-serif;">
 <h1>Tsugi Workshop Exercises</h1>
 <p>
-If you want to learn to develop for Tsugi or want to give a Tsugi Workshop, 
+If you want to learn to develop for Tsugi or want to give a Tsugi Workshop,
 here are a series of exercises that you can work through to increase your understanding of Tsugi:
 <ul>
-<li> 
+<li>
 <a href="specs/install/index.htm"
 target="_blank">
 Installing GIT and Tsugi</a></li>
-<li> 
+<li>
 <a href="specs/grade/index.htm"
 target="_blank">
 Tsugi Grade Tool</a></li>
-<li> 
+<li>
 <a href="specs/map/index.htm"
 target="_blank">
 JavaSript Map Tool</a></li>
-<li> 
+<li>
 <a href="specs/chat/index.htm"
 target="_blank">
 JQuery Chat Tool</a></li>
-<li> 
+<li>
 <a href="specs/wiscrowd/index.htm"
 target="_blank">
 Wisdom of Crowd Tool</a></li>
 </ul>
 <h1>Sample Implementations</h1>
 <p>
-You can explore working implementations of these at:
-
-<a href="https://lti-tools.dr-chuck.com/tsugi" target="_blank">https://lti-tools.dr-chuck.com/tsugi</a>
+You can explore working versions of the exercise
+sample code as well as completed solutions to these exercises at:
+<a href="https://www.tsugi.org/tsugi/dev.php"
+target="_blank">https://www.tsugi.org/tsugi/dev.php</a>
 using the Developer mode.
+The source code for these exercises is
+<a href="https://github.com/tsugiproject/tsugi-php-exercises" target="_blank">
+available in GitHub</a>.
 </p>
 </body>
 
